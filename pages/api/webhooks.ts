87897c04ff14691d4b247707175cc62b5d025b10
Dan Ward <danward@gmail.com>
2023-01-23 12:55:51 -0800

    Update webhook

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -9,7 +9,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if (req.method !== "POST") return;
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
+  }
   if (req.body[0]) {
     console.log(req.body[0]);
   }
