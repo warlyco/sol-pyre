f5993944812e4d43dedbbb647641d60b84d610ad
Dan Ward <danward@gmail.com>
2023-01-25 23:19:47 -0800

    Fix margin

--- features/featured.tsx
+++ features/featured.tsx
@@ -10,9 +10,7 @@ const project = {
 export const Featured = () => {
   return (
     <div className="bg-stone-800 py-24">
-      <div className="text-center text-3xl md:text-4xl mb-16">
-        Featured Burn
-      </div>
+      <div className="text-center text-3xl md:text-4xl mb-8">Featured Burn</div>
       <div className="flex max-w-5xl md:mx-auto border-2 border-orange-500 rounded-xl border-opacity-40 shadow-deep cursor-pointer hover:scale-[1.03] hover:shadow-deep-float mx-10 transition-all duration-300 ease-in-out">
         <Link className="flex flex-wrap" href="/burns/narentines">
           <div className="w-full md:w-1/3 flex flex-shrink-0 h-72 md:h-auto">
