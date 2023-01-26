45f04c655a5c667231c2a0c5651841c2dd9b005b
Dan Ward <danward@gmail.com>
2023-01-26 14:41:14 -0800

    Add basic about page

---
+++ pages/about.tsx
@@ -0,0 +1,60 @@
+export default function About() {
+  return (
+    <div className="flex justify-center bg-stone-900 min-h-screen relative">
+      <div className="max-w-5xl mx-auto mt-32 text-lg">
+        <h1 className="text-3xl text-center mb-4">About</h1>
+        <div className="max-w-xl mx-auto p-4 space-y-6 pb-32">
+          <p>
+            SolPyre is the first platform on Solana dedicated to the creation of
+            token burning campaings.
+          </p>
+          <p>
+            The platform was created with project creators and project managers
+            in mind. It provides an easy way to give holders custom rewards for
+            burning any NFTs of your choice.
+          </p>
+          <p>
+            <div className="mb-2">Example use cases:</div>
+            <ul className="space-y-4">
+              <li>
+                🌱&nbsp;&nbsp;&nbsp;Provide a streamlined way for users to burn
+                Gen 1 NFTs in exchange for Gen 2 mint tokens
+              </li>
+              <li>
+                🟡&nbsp;&nbsp;&nbsp;Introduce new tokens by combining multiple
+                tokens into a new one (e.g. crafting or cooking)
+              </li>
+              <li>
+                ⚠️&nbsp;&nbsp;&nbsp;Reward users for burning scam tokens aimed
+                at your project
+              </li>
+              <li>
+                👶&nbsp;&nbsp;&nbsp;Give new life to an SPL token that has
+                fallen out of use
+              </li>
+              <li>
+                😈&nbsp;&nbsp;&nbsp;Incientivize the destruction of rival
+                collection NFTs
+              </li>
+            </ul>
+          </p>
+          <p>
+            The automated campaign creation system is still under construction,
+            but a new burn campaing can currently be setup manually by the team.
+            Please{" "}
+            <a
+              className="underline text-blue-400"
+              href="https://twitter.com/sol_pyre"
+              target="_blank"
+              rel="noreferrer"
+            >
+              reach out on twitter
+            </a>{" "}
+            if you have any questions or would like to setup a new custom
+            campaign!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import { Featured } from "features/featured";
 import { Upcoming } from "features/upcoming";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <div className="flex justify-center items-center bg-stone-900 h-[90vh] relative">
         <div className="text-center flex flex-col items-center">
           <Image
@@ -17,11 +18,17 @@ export default function Home() {
           <h1 className="text-7xl md:text-8xl mb-4 uppercase tracking-widest mt-12">
             SolPyre
           </h1>
-          <h2 className="text-xl">
+          <h2 className="text-xl mb-16">
             solana &nbsp;
             <span className="text-orange-400 p-1 rounded">burning</span>
             &nbsp; services
           </h2>
+          <Link
+            href="/about"
+            className="bg-orange-500 bg-opacity-30 hover:bg-opacity-80 rounded-lg p-2 px-3 transition-all duration-300"
+          >
+            learn more
+          </Link>
         </div>
 
         <style jsx>{`
@@ -37,6 +44,6 @@ export default function Home() {
       </div>
       <Featured />
       <Upcoming />
-    </div>
+    </>
   );
 }
